7272a888e6df88b5f4fe265ccca60d03a5a8d7de
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-03-11 10:07:26 -0800

    complete looping lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const cards = [];
+
+function writeCards(name)
+{
+   for (let i = 0; i < name.length; i++) { 
+      cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+   }
+
+   return cards
+}
+
+function countDown(num)
+{
+  let i = num
+  while (i >= 0) {
+     console.log(i);
+     i--
+  }
+}
